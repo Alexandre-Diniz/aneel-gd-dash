ce7206515ed12f85824abaf9d2d322510995dfe5
Alexandre Diniz <32992263+Alexandre-Diniz@users.noreply.github.com>
2021-05-01 01:54:22 -0300

    #3

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-import pandas as pd
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.express as px
-import plotly as plt
-import json
-from urllib.request import urlopen
-from map import fig
+from dash import Dash
+from flask import Flask
 from layout import layout
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.run_server
 
 app.layout = layout
 
+
 if __name__=="__main__":
-    app.run_server(debug=False)
+    server(debug=False)
